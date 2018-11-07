9b070fb4e8becdaa8b079f7083d1f58f4522a7c8
tntmeijs <tntmeijs@gmail.com>
2018-11-07 18:22:03 +0100

    [ADD] Empty script that will handle the skateboard behavior.

---
+++ Assets/Scripts/SkateboardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkateboardController : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
