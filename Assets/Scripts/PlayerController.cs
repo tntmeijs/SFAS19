55320bf3d5f40756c473498ab772db5fae4eb1db
tntmeijs <tntmeijs@gmail.com>
2018-11-11 17:05:43 +0100

    [ADD] Private access modifier to all existing variables to keep things consistent between scripts.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,19 @@ public class PlayerController : MonoBehaviour
 
     // The character's running speed
     [SerializeField]
-    float m_RunSpeed = 5.0f;
+    private float m_RunSpeed = 5.0f;
 
     // The gravity strength
     [SerializeField]
-    float m_Gravity = 60.0f;
+    private float m_Gravity = 60.0f;
 
     // The maximum speed the character can fall
     [SerializeField]
-    float m_MaxFallSpeed = 20.0f;
+    private float m_MaxFallSpeed = 20.0f;
 
     // The character's jump height
     [SerializeField]
-    float m_JumpHeight = 4.0f;
+    private float m_JumpHeight = 4.0f;
 
     // --------------------------------------------------------------
 
@@ -27,49 +27,49 @@ public class PlayerController : MonoBehaviour
     CharacterController m_CharacterController;
 
     // The current movement direction in x & z.
-    Vector3 m_MovementDirection = Vector3.zero;
+    private Vector3 m_MovementDirection = Vector3.zero;
 
     // The current movement speed
-    float m_MovementSpeed = 0.0f;
+    private float m_MovementSpeed = 0.0f;
 
     // The current vertical / falling speed
-    float m_VerticalSpeed = 0.0f;
+    private float m_VerticalSpeed = 0.0f;
 
     // The current movement offset
-    Vector3 m_CurrentMovementOffset = Vector3.zero;
+    private Vector3 m_CurrentMovementOffset = Vector3.zero;
 
     // The starting position of the player
-    Vector3 m_SpawningPosition = Vector3.zero;
+    private Vector3 m_SpawningPosition = Vector3.zero;
 
     // Whether the player is alive or not
-    bool m_IsAlive = true;
+    private bool m_IsAlive = true;
 
     // The time it takes to respawn
-    const float MAX_RESPAWN_TIME = 1.0f;
-    float m_RespawnTime = MAX_RESPAWN_TIME;
+    private const float MAX_RESPAWN_TIME = 1.0f;
+    private float m_RespawnTime = MAX_RESPAWN_TIME;
 
     // The force added to the player (used for knockbacks)
-    Vector3 m_Force = Vector3.zero;
+    private Vector3 m_Force = Vector3.zero;
 
     // --------------------------------------------------------------
 
-    void Awake()
+    private void Awake()
     {
         m_CharacterController = GetComponent<CharacterController>();
     }
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
         m_SpawningPosition = transform.position;
     }
 
-    void Jump()
+    private void Jump()
     {
         m_VerticalSpeed = Mathf.Sqrt(m_JumpHeight * m_Gravity);
     }
 
-    void ApplyGravity()
+    private void ApplyGravity()
     {
         // Apply gravity
         m_VerticalSpeed -= m_Gravity * Time.deltaTime;
@@ -79,7 +79,7 @@ public class PlayerController : MonoBehaviour
         m_VerticalSpeed = Mathf.Min(m_VerticalSpeed, m_MaxFallSpeed);
     }
 
-    void UpdateMovementState()
+    private void UpdateMovementState()
     {
         // Get Player's movement input and determine direction and set run speed
         float horizontalInput = Input.GetAxisRaw("Horizontal_P1");
@@ -89,7 +89,7 @@ public class PlayerController : MonoBehaviour
         m_MovementSpeed = m_RunSpeed;
     }
 
-    void UpdateJumpState()
+    private void UpdateJumpState()
     {
         // Character can jump when standing on the ground
         if (Input.GetButtonDown("Jump_P1") && m_CharacterController.isGrounded)
@@ -99,7 +99,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // If the player is dead update the respawn timer and exit update loop
         if(!m_IsAlive)
@@ -127,12 +127,12 @@ public class PlayerController : MonoBehaviour
         RotateCharacterTowardsMouseCursor();
     }
 
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
     {
         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 
-    void RotateCharacter(Vector3 movementDirection)
+    private void RotateCharacter(Vector3 movementDirection)
     {
         Quaternion lookRotation = Quaternion.LookRotation(movementDirection);
         if (transform.rotation != lookRotation)
@@ -141,7 +141,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void RotateCharacterTowardsMouseCursor()
+    private void RotateCharacterTowardsMouseCursor()
     {
         Vector3 mousePosInScreenSpace = Input.mousePosition;
         Vector3 playerPosInScreenSpace = Camera.main.WorldToScreenPoint(transform.position);
@@ -151,13 +151,13 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.AngleAxis(-angle + 90.0f, Vector3.up);
     }
 
-    public void Die()
+    private public void Die()
     {
         m_IsAlive = false;
         m_RespawnTime = MAX_RESPAWN_TIME;
     }
 
-    void UpdateRespawnTime()
+    private void UpdateRespawnTime()
     {
         m_RespawnTime -= Time.deltaTime;
         if (m_RespawnTime < 0.0f)
@@ -166,14 +166,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void Respawn()
+    private void Respawn()
     {
         m_IsAlive = true;
         transform.position = m_SpawningPosition;
         transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
     }
 
-    public void AddForce(Vector3 force)
+    private public void AddForce(Vector3 force)
     {
         m_Force += force;
     }
