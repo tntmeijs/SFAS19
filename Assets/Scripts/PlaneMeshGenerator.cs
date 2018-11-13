ee47206bb09d3a4aae540025a08c0503683b6c1c
tntmeijs <tntmeijs@gmail.com>
2018-11-13 22:18:26 +0100

    [ADD] Empty script for the plane generation algorithm.

---
+++ Assets/Scripts/PlaneMeshGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMeshGenerator : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private Update()
+    {
+
+    }
+}
