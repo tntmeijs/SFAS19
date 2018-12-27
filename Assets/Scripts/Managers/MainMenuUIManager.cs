6dc0f6e71de8360d0a25699220e87475d15b706c
Tahar Meijs <tntmeijs@gmail.com>
2018-12-27 16:21:00 +0100

    Animation states
    [ADD] Animation states for various menus.
    [BUG] Transitions do not work properly yet!

--- Assets/Scripts/Managers/MainMenuUIManager.cs
+++ Assets/Scripts/Managers/MainMenuUIManager.cs
@@ -5,4 +5,46 @@ using UnityEngine.UI;
 
 public class MainMenuUIManager : MonoBehaviour
 {
+    // --------------------------------------------------------------
+
+    [Header("Animator set-up")]
+    [Header("Main menu")]
+    [SerializeField] private string m_BackName = "ClickBack";
+    [SerializeField] private string m_PlayName = "ClickPlay";
+
+    [Header("Play section")]
+    [SerializeField] private string m_TournamentName = "ClickTournament";
+
+    // --------------------------------------------------------------
+
+    private Animator m_Animator = null;
+
+    // --------------------------------------------------------------
+    
+    public void ClickBack()
+    {
+        m_Animator.SetTrigger(m_BackName);
+    }
+
+    public void ClickedPlay()
+    {
+        m_Animator.SetTrigger(m_PlayName);
+    }
+
+    public void ClickedTournament()
+    {
+        m_Animator.SetTrigger(m_TournamentName);
+    }
+
+    // --------------------------------------------------------------
+
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+
+        if (!m_Animator)
+        {
+            Debug.LogError("Animator not attached to the same object as this script!");
+        }
+    }
 }
