2509cd1e2205be5f9cdc19e84d1046331d828841
Tahar Meijs <tntmeijs@gmail.com>
2018-12-29 13:42:23 +0100

    Joining and leaving a party fixes
    [CHANGE] Joining and leaving a party works properly now.

--- Assets/Scripts/PartyManager.cs
+++ Assets/Scripts/PartyManager.cs
@@ -6,6 +6,40 @@ public class PartyManager : MonoBehaviour
 {
     // --------------------------------------------------------------
 
+    [Header("Joining a party")]
+    [SerializeField]
+    private KeyCode m_KeyboardJoinButton        = KeyCode.Return;
+
+    [SerializeField]
+    private KeyCode m_JoystickOneJoinButton     = KeyCode.Joystick1Button0;
+
+    [SerializeField]
+    private KeyCode m_JoystickTwoJoinButton     = KeyCode.Joystick2Button0;
+
+    [SerializeField]
+    private KeyCode m_JoystickThreeJoinButton   = KeyCode.Joystick3Button0;
+
+    [SerializeField]
+    private KeyCode m_JoystickFourJoinButton    = KeyCode.Joystick4Button0;
+
+    [Header("Leaving a party")]
+    [SerializeField]
+    private KeyCode m_KeyboardLeaveButton       = KeyCode.Escape;
+
+    [SerializeField]
+    private KeyCode m_JoystickOneLeaveButton    = KeyCode.Joystick1Button1;
+
+    [SerializeField]
+    private KeyCode m_JoystickTwoLeaveButton    = KeyCode.Joystick2Button1;
+
+    [SerializeField]
+    private KeyCode m_JoystickThreeLeaveButton  = KeyCode.Joystick3Button1;
+
+    [SerializeField]
+    private KeyCode m_JoystickFourLeaveButton   = KeyCode.Joystick4Button1;
+
+    // --------------------------------------------------------------
+
     // Maps the controller to the player index
     // List[0] --> player 1
     // List[1] --> player 2
@@ -13,6 +47,7 @@ public class PartyManager : MonoBehaviour
     // List[3] --> player 4
     private List<int> m_RegisteredControllersIndices = new List<int>();
 
+    // A keyboard is considered a controller as well!
     private enum Controllers
     {
         // Starts at -2 to keep the enum mapping simple when converting to integers in the code below
@@ -27,16 +62,7 @@ public class PartyManager : MonoBehaviour
         TotalNumberOfControllers    // Always equal to the number of controllers if this kind of enum style is used
     }
 
-    private void OnGUI()
-    {
-        for (int i = 0; i < m_RegisteredControllersIndices.Count; ++i)
-        {
-            if (m_RegisteredControllersIndices[i] == (int)Controllers.Invalid)
-                GUI.Label(new Rect(200, 20 * i, 200, 20), "Player " + i + " & index invalid");
-            else
-                GUI.Label(new Rect(200, 20 * i, 200, 20), "Player " + i + " & index " + m_RegisteredControllersIndices[i]);
-        }
-    }
+    // --------------------------------------------------------------
 
     private void Awake()
     {
@@ -48,43 +74,49 @@ public class PartyManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        PollPartyJoinButtons();
+        PollPartyLeaveButtons();
+    }
+
+    private void PollPartyJoinButtons()
+    {
+        if (Input.GetKeyDown(m_KeyboardJoinButton))
         {
             AddGameController(Controllers.Keyboard);
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        if (Input.GetKeyDown(m_JoystickOneJoinButton))
         {
             AddGameController(Controllers.Joystick0);
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+        if (Input.GetKeyDown(m_JoystickTwoJoinButton))
         {
             AddGameController(Controllers.Joystick1);
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick3Button0))
+        if (Input.GetKeyDown(m_JoystickThreeJoinButton))
         {
             AddGameController(Controllers.Joystick2);
         }
 
-        if (Input.GetKeyDown(KeyCode.Joystick4Button0))
+        if (Input.GetKeyDown(m_JoystickFourJoinButton))
         {
             AddGameController(Controllers.Joystick3);
         }
     }
 
-    void AddGameController(Controllers type)
+    private void AddGameController(Controllers type)
     {
         // The controller that is being added exceeds the maximum number of controllers allowed, no need to continue
         if (m_RegisteredControllersIndices.Count > (int)Controllers.TotalNumberOfControllers)
             return;
 
-        foreach (int controllerIndex in m_RegisteredControllersIndices)
+        for (int controllerIndex = 0; controllerIndex < (int)Controllers.TotalNumberOfControllers; ++controllerIndex)
         {
-            if (controllerIndex == (int)type)
+            if (m_RegisteredControllersIndices[controllerIndex] == (int)type)
             {
-                // Controller already in use
+                // This controller exists already, no need to continue
                 return;
             }
         }
@@ -100,17 +132,46 @@ public class PartyManager : MonoBehaviour
         }
     }
 
-    void RemoveGameController(int index)
+    private void PollPartyLeaveButtons()
+    {
+        if (Input.GetKeyDown(m_KeyboardLeaveButton))
+        {
+            RemoveGameController(Controllers.Keyboard);
+        }
+
+        if (Input.GetKeyDown(m_JoystickOneLeaveButton))
+        {
+            RemoveGameController(Controllers.Joystick0);
+        }
+
+        if (Input.GetKeyDown(m_JoystickTwoLeaveButton))
+        {
+            RemoveGameController(Controllers.Joystick1);
+        }
+
+        if (Input.GetKeyDown(m_JoystickThreeLeaveButton))
+        {
+            RemoveGameController(Controllers.Joystick2);
+        }
+
+        if (Input.GetKeyDown(m_JoystickFourLeaveButton))
+        {
+            RemoveGameController(Controllers.Joystick3);
+        }
+    }
+
+    private void RemoveGameController(Controllers type)
     {
         // No more controllers left to unregister
         if (m_RegisteredControllersIndices.Count < 1)
             return;
-        
-        foreach (int controllerIndex in m_RegisteredControllersIndices)
+
+        for (int controllerIndex = 0; controllerIndex < (int)Controllers.TotalNumberOfControllers; ++controllerIndex)
         {
-            if (controllerIndex == index)
+            if (m_RegisteredControllersIndices[controllerIndex] == (int)type)
             {
                 m_RegisteredControllersIndices[controllerIndex] = (int)Controllers.Invalid;
+                break;  // Only set one controller, if the others are invalid, leave them for the next controller
             }
         }
     }
