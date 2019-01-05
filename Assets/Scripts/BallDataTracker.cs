ccf6117d9efa6f928ac862a23c967e4de470e43a
Tahar Meijs <tntmeijs@gmail.com>
2019-01-05 20:03:35 +0100

    Ball data
    [ADD] Script used to store data relevant to the ball interaction. Data such as the last player that touched the ball.

---
+++ Assets/Scripts/BallDataTracker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDataTracker : MonoBehaviour
+{
+    // --------------------------------------------------------------
+
+    [Header("Car tag")]
+    // Tag associated with the cars
+    [SerializeField]
+    private string m_CarTag = "Car";
+
+    // --------------------------------------------------------------
+
+    // Last player that managed to touch the ball
+    private Global.Player m_LastPlayer;
+
+    // --------------------------------------------------------------
+    
+    public Global.Player GetLastPlayerThatTouchedBall()
+    {
+        return m_LastPlayer;
+    }
+
+    // --------------------------------------------------------------
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == m_CarTag)
+            m_LastPlayer = collision.gameObject.GetComponent<PlayerID>().GetPlayerID();
+    }
+}
