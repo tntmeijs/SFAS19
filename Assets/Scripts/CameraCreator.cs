c0eed338af91bfc9b2781a9e6de1cd8d37694c47
Tahar Meijs <tntmeijs@gmail.com>
2019-01-05 20:12:54 +0100

    Empty camera setup class
    [ADD] Empty script file that will hold the camera creation code.

---
+++ Assets/Scripts/CameraCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCreator : MonoBehaviour
+{
+    // --------------------------------------------------------------
+
+    // --------------------------------------------------------------
+
+    // Create a split-screen set-up based on the number of real players
+    public void CreateCameras(int numberOfRealPlayers)
+    {
+    }
+}
