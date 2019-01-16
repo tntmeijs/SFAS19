fd2b6942dc8e6f8a2b0637ad800c400aa06fb0d5
Tahar Meijs <tntmeijs@gmail.com>
2019-01-16 16:16:33 +0100

    Basic state machine set-up
    [ADD] Animator state machine.
    [ADD] Empty state machine states.

---
+++ Assets/Scripts/AI/StateMachine/AttackOpponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackOpponent : StateMachineBehaviour
+{
+    private void OnStateEnter()
+    {
+        //
+    }
+
+    private void OnStateExit()
+    {
+        //
+    }
+
+    private void OnDisable()
+    {
+        //
+    }
+
+    private void OnStateUpdate()
+    {
+        //
+    }
+}
---
+++ Assets/Scripts/AI/StateMachine/TakeOverOpponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeOverOpponent : StateMachineBehaviour
+{
+    private void OnStateEnter()
+    {
+        //
+    }
+
+    private void OnStateExit()
+    {
+        //
+    }
+
+    private void OnDisable()
+    {
+        //
+    }
+
+    private void OnStateUpdate()
+    {
+        //
+    }
+}
