43821b0f2add87e31dd53f55da49813d4de9ae12
Tahar Meijs <tntmeijs@gmail.com>
2019-01-16 16:28:48 +0100

    State machine names
    [ADD] AI state machine parameter names.
    [DELETE] Unused code in the AI controller.
    [CHANGE] AI state machine flow.

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -6,44 +6,16 @@ public class AIController : MonoBehaviour
 {
     // --------------------------------------------------------------
 
-    // --------------------------------------------------------------
-
-    // The current movement direction in x & z.
-    Vector3 m_MovementDirection = Vector3.zero;
-
     // Whether the player is alive or not
     bool m_IsAlive = true;
 
     // --------------------------------------------------------------
 
-    void Awake()
+    private void Update()
     {
-    }
-
-    void Start()
-    {
-    }
-
-    void Update()
-    {
-        // If the player is dead update the respawn timer and exit update loop
         if (!m_IsAlive)
         {
             return;
-        }
-    }
-
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-    }
-
-    void RotateCharacter(Vector3 movementDirection)
-    {
-        Quaternion lookRotation = Quaternion.LookRotation(movementDirection);
-        if (transform.rotation != lookRotation)
-        {
-            transform.rotation = lookRotation;
         }
     }
 
