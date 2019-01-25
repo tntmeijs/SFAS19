425e3f9b730c4a2f915219e3e1f086974555dcce
Tahar Meijs <tntmeijs@gmail.com>
2019-01-25 22:25:10 +0100

    Power-up manager
    [ADD] Manager class that will handle all the power-up related interactions.
    [ADD] Code to collect power-ups.

--- Assets/Scripts/PowerUps/PowerupBase.cs
+++ Assets/Scripts/PowerUps/PowerupBase.cs
@@ -7,16 +7,61 @@ public class PowerupBase : MonoBehaviour
 {
     // --------------------------------------------------------------
 
+    [Header("Configuration")]
+    // Name of the tag associated with the racing cars
     [SerializeField]
     private string m_CarTag = "Car";
 
-    // --------------------------------------------------------------
+    // What kind of power-up is this?
+    [SerializeField]
+    private PowerupManager.PowerupType m_PowerUpType = PowerupManager.PowerupType.None;
+
+    // Time until the power-up reappears again
+    [SerializeField]
+    private float m_PowerUpCooldown = 2.0f;
 
     // --------------------------------------------------------------
+
+    private SphereCollider m_Trigger = null;
+    private GameObject m_MeshContainer = null;
+
+    // --------------------------------------------------------------
+
+    private void Awake()
+    {
+        // No need to check if this is valid, it always succeeds because this script REQUIRES this component
+        m_Trigger = GetComponent<SphereCollider>();
+
+        // The mesh container object is the first child of the root (which holds this script)
+        m_MeshContainer = transform.GetChild(0).gameObject;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag(m_CarTag))
-            Destroy(gameObject);
+        // The car has a collider attached to the body, which lives in a container, which is a child of the root
+        // So like this: collider >>> container >>> root
+        var carRoot = other.transform.parent.parent.gameObject;
+
+        if (carRoot.CompareTag(m_CarTag))
+        {
+            // Let the player know which type of power-up they collected
+            carRoot.gameObject.GetComponent<PowerupManager>().SetPowerUp(m_PowerUpType);
+
+            // Hide the power-up until the time-out finishes
+            StartCoroutine(PowerUpTimeOut());
+        }
+    }
+
+    private IEnumerator PowerUpTimeOut()
+    {
+        // Disable the mesh of the object and the collider on the root
+        m_MeshContainer.SetActive(false);
+        m_Trigger.enabled = false;
+
+        yield return new WaitForSeconds(m_PowerUpCooldown);
+
+        // Revert the object back to its default state
+        m_MeshContainer.SetActive(true);
+        m_Trigger.enabled = true;
     }
 }
---
+++ Assets/Scripts/PowerUps/PowerupManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    // --------------------------------------------------------------
+
+    private PowerupType m_CurrentPowerUp = PowerupType.None;
+
+    // --------------------------------------------------------------
+
+    public enum PowerupType
+    {
+        None = -1,
+
+        Booster,
+        Missiles,
+
+        TotalNumberOfTypes
+    }
+
+    public void SetPowerUp(PowerupType type)
+    {
+        m_CurrentPowerUp = type;
+    }
+
+    // --------------------------------------------------------------
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
