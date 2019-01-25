33bd24c326507425a60f3b4b267ed293a509bd7d
Tahar Meijs <tntmeijs@gmail.com>
2019-01-25 20:57:32 +0100

    Booster power-up model
    [ADD] Blender boost power-up model.

---
+++ Assets/Scripts/PowerUps/PowerupBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class PowerupBase : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
