ee33ab6a3b8e6cf55d47118192482a7dcab3680d
Tahar Meijs <tntmeijs@gmail.com>
2019-01-27 22:06:23 +0100

    Scoreboard scene loading
    [ADD] Fade-out animation.
    [ADD] Code to load the scoreboard after the fade-out.

--- Assets/Scripts/LapTracker.cs
+++ Assets/Scripts/LapTracker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LapTracker : MonoBehaviour
 {
@@ -21,10 +22,22 @@ public class LapTracker : MonoBehaviour
     [SerializeField]
     private Color m_WinTextColor = Color.green;
 
+    // Show the scoreboard after this many seconds
+    [SerializeField]
+    private float m_ScoreboardShowTimeout = 3.0f;
+
+    // Name of the animation trigger that starts the fade-out to the scoreboard scene
+    [SerializeField]
+    private string m_FadeOutAnimationTrigger = "StartFade";
+
     [Header("References")]
     // Way point container holding the ideal racing line
     [SerializeField]
     private Transform m_WaypointContainer = null;
+
+    // Canvas that holds the lap information UI as well as the fade-out animation
+    [SerializeField]
+    private Animator m_FadeOutLapInfoCanvasAnimator = null;
 
     // Text that holds the lap information UI for player one to four
     [SerializeField]
@@ -66,7 +79,8 @@ public class LapTracker : MonoBehaviour
             !m_PlayerLapCounters[1] ||
             !m_PlayerLapCounters[2] ||
             !m_PlayerLapCounters[3] ||
-            !m_WaypointContainer)
+            !m_WaypointContainer    ||
+            !m_FadeOutLapInfoCanvasAnimator)
             Debug.LogError("Error: not all references have been set correctly.");
     }
 
@@ -90,7 +104,7 @@ public class LapTracker : MonoBehaviour
             if (place == -1)
                 return;
 
-            m_PlayerLapCounters[playerIndex].text = "Place " + (place + 1) + "/" + Global.MaximumNumberOfPlayers;
+            m_PlayerLapCounters[playerIndex].text = "Player " + playerIndex + " placed #" + (place + 1) + "/" + Global.MaximumNumberOfPlayers;
             m_PlayerLapCounters[playerIndex].color = m_WinTextColor;
         }
         else
@@ -116,5 +130,29 @@ public class LapTracker : MonoBehaviour
 
         // Save the player in the scoreboard
         m_FinishedPlayers.Add(id);
+
+        // Move on to the final scoreboard once the last player finishes
+        if (m_FinishedPlayers.Count == Global.MaximumNumberOfPlayers)
+            StartCoroutine(ViewScoreboard());
+    }
+
+    private IEnumerator ViewScoreboard()
+    {
+        yield return new WaitForSeconds(m_ScoreboardShowTimeout);
+
+        // Start the animation to make everything black
+        m_FadeOutLapInfoCanvasAnimator.SetTrigger(m_FadeOutAnimationTrigger);
+
+        // Do the actual scoreboard scene loading
+        StartCoroutine(LoadScoreboard());
+    }
+
+    private IEnumerator LoadScoreboard()
+    {
+        // Wait until the fade animation has finished fading out before switching to the scoreboard scene
+        yield return new WaitForSeconds(m_FadeOutLapInfoCanvasAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.length);
+
+        // The scoreboard is the next scene in the build order
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
