b8578f71bc38816a108d36eab673b72c040b74a4
Tahar Meijs <tntmeijs@gmail.com>
2019-01-27 17:52:36 +0100

    Reset detector script set-up
    [ADD] Script that will detect whether a car needs to be reset.
    [ADD] Empty methods that will make up the functionality of the class.

---
+++ Assets/Scripts/ResetDetector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ResetDetector : MonoBehaviour
+{
+    // --------------------------------------------------------------
+
+    [Header("Configuration")]
+    // Time between checks in seconds whether the car is no longer moving correctly
+    [SerializeField]
+    private float m_CheckDelay = 5.0f;
+
+    // --------------------------------------------------------------
+
+    // Object that contains the ideal racing line way points
+    private Transform m_WaypointContainer = null;
+
+    // --------------------------------------------------------------
+
+    public void SetWaypointContainer(Transform container)
+    {
+        m_WaypointContainer = container;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ResetCarLoop());
+    }
+
+    private IEnumerator ResetCarLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(m_CheckDelay);
+
+            // If the car is in some kind of trouble, reset its position and orientation
+            if (CarShouldBeReset())
+                ResetVehicle();
+        }
+    }
+
+    private bool CarShouldBeReset()
+    {
+        return false;
+    }
+
+    private void ResetVehicle()
+    {
+
+    }
+}
