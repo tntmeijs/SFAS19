16f211a17d1ec153092cdd95b14a941ff4a3e1e0
Tahar Meijs <tntmeijs@gmail.com>
2019-01-27 18:32:14 +0100

    Player controller flip resetting
    [ADD] Player controlled cars will now be flipped and placed near a way point once they are upside-down.

--- Assets/Scripts/ResetDetector.cs
+++ Assets/Scripts/ResetDetector.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class ResetDetector : MonoBehaviour
 {
     // --------------------------------------------------------------
@@ -43,11 +42,44 @@ public class ResetDetector : MonoBehaviour
 
     private bool CarShouldBeReset()
     {
-        return false;
+        // If the angle of the car with the ground is greater than 135 degrees, assume it is upside-down
+        return Vector3.Dot(transform.up, Vector3.down) > 0.7071f;
     }
 
     private void ResetVehicle()
     {
+        // Find the closest node in the way point system
+        AIController aIController = GetComponent<AIController>();
 
+        if (!aIController)
+        {
+            // By default, the closest node is the start node
+            int closestNodeIndex = 0;
+
+            // Closest distance to a node
+            float closestDistanceFoundToNode = -1.0f;
+
+            // This is a player, so we just place the car at the nearest track node
+            for (int i = 0; i < m_WaypointContainer.childCount; ++i)
+            {
+                float distanceToNode = Vector3.Distance(m_WaypointContainer.GetChild(i).position, transform.position);
+
+                // If this is a shorter distance or the distance has never been set yet, record this distance
+                if (distanceToNode < closestDistanceFoundToNode ||
+                    closestDistanceFoundToNode == -1.0f)
+                {
+                    closestNodeIndex = i;
+                    closestDistanceFoundToNode = distanceToNode;
+                }
+            }
+
+            // Place the car back on track
+            transform.position = m_WaypointContainer.GetChild(closestNodeIndex).position + Vector3.up;
+            transform.rotation = Quaternion.identity;
+        }
+        else
+        {
+            // This is an AI player, so use the node data in the AI controller to place it back on track
+        }
     }
 }
