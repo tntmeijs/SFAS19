6e87d52b1a80ddcdc2326bf6ad06947488827add
Tahar Meijs <tntmeijs@gmail.com>
2019-01-27 20:32:38 +0100

    Laps are now being counted whenever the trigger is passed
    [ADD] Trigger event.
    [ADD] Lap counters.

--- Assets/Scripts/LapCounter.cs
+++ Assets/Scripts/LapCounter.cs
@@ -4,4 +4,36 @@ using UnityEngine;
 
 public class LapCounter : MonoBehaviour
 {
+    // --------------------------------------------------------------
+
+    [Header("Configuration")]
+    // Tag associated with the racing cars
+    [SerializeField]
+    private string m_CarTag = "Car";
+
+    // --------------------------------------------------------------
+
+    public delegate void CarPassesLapCounterVolume(Global.Player playerID);
+    public CarPassesLapCounterVolume OnCarFinishesLap;
+
+    // --------------------------------------------------------------
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // A car just passed the trigger volume
+        if (other.gameObject.CompareTag(m_CarTag))
+        {
+            // Try to let any listeners know about the player that finished a lap
+            try
+            {
+                // Retrieve the player ID (component is attached to the top-most object)
+                OnCarFinishesLap(other.transform.root.GetComponent<PlayerID>().GetPlayerID());
+            }
+            catch (System.NullReferenceException e)
+            {
+                // Ignore the exception, this only happens when there are no listeners attached to the delegate,
+                // no big deal...
+            }
+        }
+    }
 }
--- Assets/Scripts/LapTracker.cs
+++ Assets/Scripts/LapTracker.cs
@@ -4,4 +4,48 @@ using UnityEngine;
 
 public class LapTracker : MonoBehaviour
 {
+    // --------------------------------------------------------------
+
+    [Header("Configuration")]
+    // Object that tracks the laps
+    [SerializeField]
+    private LapCounter m_LapCounter;
+
+    // --------------------------------------------------------------
+
+    // Keep track of the number of laps per player
+    private int[] m_LapsCompleted = new int[Global.MaximumNumberOfPlayers];    
+
+    // --------------------------------------------------------------
+
+    private void Awake()
+    {
+        CheckNullReferences();
+        SetInitialCounterValues();
+
+        // Increment the lap counter and update the UI accordingly when the player finishes a lap
+        m_LapCounter.OnCarFinishesLap += playerID => {
+            ++m_LapsCompleted[(int)playerID];
+            UpdateLapVisualization();
+        };
+    }
+
+    private void CheckNullReferences()
+    {
+        // Check if the reference has been set
+        if (!m_LapCounter)
+            Debug.LogError("Error: lap counter object not set.");
+    }
+
+    private void SetInitialCounterValues()
+    {
+        // Since the players will trigger the lap counter as soon as the race stars, we have to compensate for it by
+        // initializaing the lap counters to -1 instead of 0.
+        for (int i = 0; i < Global.MaximumNumberOfPlayers; ++i)
+            m_LapsCompleted[i] = -1;
+    }
+
+    private void UpdateLapVisualization()
+    {
+    }
 }
